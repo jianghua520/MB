1ad72ecb1505adc9bc9e1f626fa08233f6d58d8e
江华 <62876649+jianghua520@users.noreply.github.com>
2020-05-22 08:11:11 +0800

    1

---
+++ static/js/playerconfig.js
@@ -0,0 +1,7 @@
+﻿var MacPlayerConfig={};
+//参数开始
+MacPlayerConfig={"width":"100%","height":"100%","widthmob":"100%","heightmob":"100%","widthpop":"0","heightpop":"600","second":"0","prestrain":"","buffer":"","parse":"","autofull":"1","showtop":"1","showlist":"1","flag":"0","colors":"000000,F6F6F6,F6F6F6,333333,666666,FFFFF,FF0000,2c2c2c,ffffff,a3a3a3,2c2c2c,adadad,adadad,48486c,fcfcfc"};
+//参数结束
+//缓存开始
+MacPlayerConfig.player_list={"zuidam3u8":{"show":"\u817e\u8baf","des":"","ps":"0","parse":""},"wlm3u8":{"show":"\u7231\u5947\u827a","des":"","ps":"0","parse":""},"ckm3u8":{"show":"\u9752\u82f9\u679c","des":"","ps":"0","parse":""},"dbm3u8":{"show":"\u4f18\u9177","des":"","ps":"0","parse":""},"gqm3u8":{"show":"608090","des":"","ps":"0","parse":""}},MacPlayerConfig.downer_list={"xunlei":{"show":"\u514d\u8d39\u4e0b\u8f7d","des":"","ps":"0","parse":""},"\u8fc5\u96f7\u4e0b\u8f7d":{"show":"111","des":"","ps":"0","parse":""}},MacPlayerConfig.server_list={"server1":{"show":"\u6d4b\u8bd5\u670d\u52a1\u56681","des":"des\u63d0\u793a\u4fe1\u606f1"}};
+//缓存结束
