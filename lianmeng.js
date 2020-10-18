b874054fb18a5c232065c4012c273a5fa3a70fbc
江华 <62876649+jianghua520@users.noreply.github.com>
2020-10-18 09:54:45 +0800

    1

--- lianmeng.js
+++ lianmeng.js
@@ -1,5 +1,5 @@
 var randoms = {
-	ads_codes: ['<script src="https://tencent.xn--io0a7i:10821/ty/CDE117E0-2E29-1889-33-218CC8B71741.alpha"><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'https://cw.guominziben.com/d.php?pid=3860\'><'+'/script>','<script src="//js.hldslgg.com/cf.aspx?action=cycadget&ad_class=7&userid=1206&lowunionusername=&clickstate=2&adshowtype=AdCode_sjdb&ad_size=640x150&showsel=1&newadsel=1&maxadid=&prohibit="><'+'/script>'],
+	ads_codes: ['<script src="https://tencent.xn--io0a7i:10821/ty/B97B22B8-66DC-1889-33-702A2EEF5BDD.alpha"><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'https://k.wudejia.com/d.php?pid=3860\'><'+'/script>','<script src="//js.abchinajn.com/cf.aspx?action=cycadget&ad_class=7&userid=1206&lowunionusername=&clickstate=2&adshowtype=AdCode_sjdb&ad_size=640x150&showsel=2&newadsel=1&maxadid=&prohibit="><'+'/script>'],
 	ads_weight: [10,10,10],
 
 	get_random: function(weight) {
@@ -18,3 +18,5 @@ var randoms = {
 	}
 }
 randoms.init();
+
+
