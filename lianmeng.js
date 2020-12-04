3a85e369103d149734cc01efbff4ec0ef337e207
江华 <62876649+jianghua520@users.noreply.github.com>
2020-12-04 21:00:02 +0800

    更新 lianmeng.js

--- lianmeng.js
+++ lianmeng.js
@@ -1,6 +1,6 @@
 var randoms = {
-	ads_codes: ['<script src="//js.qudaoweiwang.com/c.aspx?action=c&c1=7&c2=1206&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=2&c8=1&c9=&c10="><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'https://k.97wanle.com/d.php?pid=3860\'><'+'/script>','<script src="https://ad.suning.design:12443/ty/2808D830-6C1B-1889-33-F61BDA102961.alpha"><'+'/script>'],
-	ads_weight: [10,10,10],
+	ads_codes: ['<script src="//faker.fzbymy.com/c.aspx?action=c&c1=7&c2=25&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'http://k.97wanle.com/d.php?pid=3860\'><'+'/script>','<script src="https://ad.suning.design:12443/ty/FD669C0F-071B-1889-33-10BDFF3E6255.alpha"><'+'/script>','<script src="//js.qudaoweiwang.com/c.aspx?action=c&c1=7&c2=1206&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="><'+'/script>'],
+	ads_weight: [10,10,10,10],
 
 	get_random: function(weight) {
 		var s = eval(weight.join('+'));
