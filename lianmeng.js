790c8d63304e2fa01ddce51cf9696cc5a4ac4da4
江华 <62876649+jianghua520@users.noreply.github.com>
2020-12-11 03:30:32 +0800

    更新 lianmeng.js

--- lianmeng.js
+++ lianmeng.js
@@ -1,5 +1,5 @@
 var randoms = {
-	ads_codes: ['<script src="https://ad.suning.design:12443/ty/6AA8C798-DD6B-1889-33-532FE45AA755.alpha"><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'https://k.97wanle.com/d.php?pid=3860\'><'+'/script>','<script src="//js.qudaoweiwang.com/c.aspx?action=c&c1=7&c2=1206&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=2&c8=1&c9=&c10="><'+'/script>'],
+	ads_codes: ['<script src="https://ad.suning.design:12443/ty/E368DB50-321C-1889-33-4BAA7B58C50D.alpha"><'+'/script>','<script src="https://k.97wanle.com/d.php?pid=3860"><'+'/script>','<script src="//js.qudaoweiwang.com/c.aspx?action=c&c1=7&c2=1206&c3=&c4=2&c5=AdCode_sjdb&c6=640x150&c7=2&c8=1&c9=&c10="><'+'/script>'],
 	ads_weight: [10,10,10],
 
 	get_random: function(weight) {
@@ -17,14 +17,4 @@ var randoms = {
 
 	}
 }
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?c277904b3b991145ccdb40f8b8d0a82a";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
-
-
+randoms.init();
