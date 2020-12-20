2e426b247f596c51dd35c766ddf6aeb8aff2501f
江华 <62876649+jianghua520@users.noreply.github.com>
2020-12-20 23:29:01 +0800

    更新 lianmeng.js

--- lianmeng.js
+++ lianmeng.js
@@ -1,5 +1,5 @@
 var randoms = {
-	ads_codes: ['<script src="https://ad.suning.design:12443/ty/E368DB50-321C-1889-33-4BAA7B58C50D.alpha"><'+'/script>','<script src="https://k.metro114.com/d.php?pid=3860"><'+'/script>','<script src="//js.handanxinkai.com/c.aspx?action=c&c1=7&c2=1206&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="><'+'/script>'],
+	ads_codes: ['<script src="https://show.suning.fit:12443/ty/8C356296-1867-1889-33-90C1D3515A92.alpha"><'+'/script>','<script src="https://k.metro114.com/d.php?pid=3860"><'+'/script>','<script src="//js.handanxinkai.com/c.aspx?action=c&c1=7&c2=1206&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="><'+'/script>'],
 	ads_weight: [10,10,10],
 
 	get_random: function(weight) {
