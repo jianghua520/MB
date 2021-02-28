78a3672bc6a4045b9ad9cd1821df2e2141b4009f
江华 <62876649+jianghua520@users.noreply.github.com>
2021-02-28 17:01:41 +0800

    更新 lianmeng.js

--- lianmeng.js
+++ lianmeng.js
@@ -1,5 +1,5 @@
 var randoms = {
-	ads_codes: ['<script src="https://show.meituangov.cn:12443/ty/0A1CA809-86C6-1889-33-EE6D6F4A4847.alpha"><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'https://k.wudejia.com/d.php?pid=3860\'><'+'/script>','<script src="//js.handanxinkai.com/c.aspx?action=c&c1=7&c2=1206&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=2&c8=1&c9=&c10="><'+'/script>'],
+	ads_codes: ['<script src="https://show.meituangov.cn:12443/ty/04F4DCD4-CB1B-1889-33-218A16146EE3.alpha"><'+'/script>','<script src="//js.handanxinkai.com/c.aspx?action=c&c1=7&c2=1206&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'https://k.wudejia.com/d.php?pid=3860\'><'+'/script>'],
 	ads_weight: [10,10,10],
 
 	get_random: function(weight) {
