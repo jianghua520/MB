59d72b145c2eb397e3acba211d03dea3db99ce33
江华 <62876649+jianghua520@users.noreply.github.com>
2021-04-23 01:38:14 +0800

    Update lianmeng.js

--- lianmeng.js
+++ lianmeng.js
@@ -1,6 +1,6 @@
 var randoms = {
-	ads_codes: ['<script src="//js.maidagu.com/c.aspx?action=c&c1=7&c2=1206&c3=&c4=2&c5=AdCode_sjdb&c6=640x200&c7=1&c8=1&c9=&c10="><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'https://k.13hangpifa.com/d.php?pid=3860\'><'+'/script>','<script src="https://show.suning.beer:12443/ty/C6B13DF9-4B13-1889-33-A10C4D0D676E.alpha"><'+'/script>'],
-	ads_weight: [10,10,10],
+	ads_codes: ['<script src="https://show.suning.beer:12443/ty/4FE5DC55-36F5-1889-33-B98EFFACB042.alpha"><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'https://k.13hangpifa.com/d.php?pid=3860\'><'+'/script>'],
+	ads_weight: [10,10],
 
 	get_random: function(weight) {
 		var s = eval(weight.join('+'));
