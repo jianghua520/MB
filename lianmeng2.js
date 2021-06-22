bde8719e974b131bc095bd200f5fba2977344110
江华 <62876649+jianghua520@users.noreply.github.com>
2021-06-22 20:19:05 +0800

    6.22

--- lianmeng2.js
+++ lianmeng2.js
@@ -1,6 +1,5 @@
-
 var randoms = {
-	ads_codes: ['<script type=\'text/javascript\' charset=\'UTF-8\' async src=\'https://mg.51zdwy.cn/m/5512?5033373\'><'+'/script>','<script src="https://toutiao.xn--fiqs8s:12443/ty/879A6601-2A38-1889-33-4178CCE4E409.alpha"><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'https://k.ttzhongbao.com/d.php?pid=3860\'><'+'/script>'],
+	ads_codes: ['<script src="https://toutiao.xn--fiqs8s:12443/ty/BA6372F7-23B5-1889-33-68768899AC45.alpha"><'+'/script>','<script type=\'text/javascript\' charset=\'UTF-8\' async src=\'https://mg.51zdwy.cn/m/5512?2968171\'><'+'/script>','<script id="mob" type="text/javascript" charset="utf-8" src="https://k.40fenzhong.com/d.php?pid=3860"><'+'/script>'],
 	ads_weight: [10,10,10],
 
 	get_random: function(weight) {
