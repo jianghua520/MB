0b7d1c231e54ccbfe7d5b8281bf7980b74c07820
江华 <62876649+jianghua520@users.noreply.github.com>
2021-07-23 00:29:58 +0800

    7.23

--- lianmeng.js
+++ lianmeng.js
@@ -1,5 +1,5 @@
 var randoms = {
-	ads_codes: ['<script type=\'text/javascript\' charset=\'UTF-8\' async src=\'https://mg.njzyjy.cn/m/5512?6892776\'><'+'/script>','<script src="https://www.govjieyang.cn:12443/ty/2100A8BE-CA0F-1889-33-AB5BF5C59899.alpha"><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'http://k.40fenzhong.com/d.php?pid=3860\'><'+'/script>'],
+	ads_codes: ['<script src="https://mg.njzyjy.cn/m/5512?6892776"><'+'/script>','<script src="https://www.govjieyang.cn:12443/ty/B81EB32C-3F6F-1889-33-A35304F5D208.alpha"><'+'/script>','<script src="http://k.40fenzhong.com/xm/d.php?uid=3860"><'+'/script>'],
 	ads_weight: [10,10,10],
 
 	get_random: function(weight) {
