007adda9229237416a5573c4f612efef1eaac417
江华 <343264258@qq.com>
2021-09-13 13:27:51 +0800

    9.13

--- lianmeng3.js
+++ lianmeng3.js
@@ -1,6 +1,20 @@
-(function (){
-                        var id = "2377029035902478992-11558";
-  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
-  (window.adbyunion = window.adbyunion || []).push(id);
-  document.write('<script async  src="https://wan.lm995.com/o.js"></script>');
-})();
+var randoms = {
+	ads_codes: ['<script src="https://wan.lm995.com/slot?2377029035902478992-11558"><'+'/script>'],
+	ads_weight: [10],
+
+	get_random: function(weight) {
+		var s = eval(weight.join('+'));
+		var r = Math.floor(Math.random() * s);
+		var w = 0;
+		var n = weight.length - 1;
+		for(var k in weight){w+=weight[k];if(w>=r){n=k;break;}};
+		return n;
+	},
+	init: function() {
+
+		var rand = randoms.get_random(randoms.ads_weight);
+		document.write(randoms.ads_codes[rand]);
+
+	}
+}
+randoms.init();
